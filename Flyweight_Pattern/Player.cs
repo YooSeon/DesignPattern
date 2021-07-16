5398e8e9b59521b2b6be7c658ce470a424fb6214
YooSeon <nate963@nate.com>
2021-07-16 19:20:47 +0900

    Flyweight_Pattern

---
+++ Flyweight_Pattern/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int playerX;
+    [SerializeField] int playerZ;
+    void Update()
+    {
+        MoveToPlayer();
+    }
+    void MoveToPlayer()
+    {
+        playerX = Mathf.Clamp(Mathf.RoundToInt(this.transform.position.x),0,2); 
+        playerZ = Mathf.Clamp(Mathf.RoundToInt(this.transform.position.z), 0, 2);
+
+        float _terrain = World.inst.GetTile(playerX, playerZ).GetMoveSpeed() * Time.deltaTime;
+        Debug.Log("NowSpeed :" + World.inst.GetTile(playerX, playerZ).GetMoveSpeed());
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3( -_terrain,0,0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(_terrain, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += new Vector3(0, 0, _terrain);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position += new Vector3(0, 0, -_terrain);
+        }
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, -0.5f , 2.5f),0,Mathf.Clamp(this.transform.position.z, -0.5f, 2.5f));
+    }
+}
---
+++ Flyweight_Pattern/World.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain
+{
+    public Terrain(float _moveSpeed , bool _isWater)
+    {
+        moveSpeed = _moveSpeed;
+        isWater = _isWater;
+    }
+    public float GetMoveSpeed() => moveSpeed;
+    public bool IsWater() => isWater;
+
+    private float moveSpeed;
+    private bool isWater;
+}
+
+public class World : MonoBehaviour
+{
+    public static World inst;
+    [SerializeField] Material[] mat;
+    Terrain[,] tiles = new Terrain[3,3];
+    Terrain grassTerrain = new Terrain(3,false);
+    Terrain hillTerrain = new Terrain(1,false);
+    Terrain riverTerrain = new Terrain(0.5f,true);
+    public Terrain GetTile(int x, int y) => tiles[x,y];
+
+    void Awake()
+    {
+        inst = this;
+        CreateTile();
+    }
+
+    void CreateTile()
+    {
+        for(int x = 0 ; x < 3; x++)
+        {
+            for(int z = 0 ; z < 3; z++)
+            {
+                GameObject _ob = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                _ob.transform.position = new Vector3(x,-1,z);
+                if(z == 0) tiles[x,z] = grassTerrain;
+                else if(z == 1) tiles[x,z] = riverTerrain;
+                else if(z == 2) tiles[x,z] = hillTerrain;
+
+                _ob.GetComponent<MeshRenderer>().material = mat[z];
+                _ob.transform.SetParent(this.transform);
+            }
+        }
+    }
+
+
+
+}
